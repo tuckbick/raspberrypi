403c2e1e00f40bcb6dbbfe042df324a86d62902f
Tucker Bickler <tbickler@homeaway.com>
2017-01-06 02:06:13 -0600

    adding package.json and flicker.py

---
+++ flicker.py
@@ -0,0 +1,46 @@
+import RPi.GPIO as GPIO import time
+import random
+
+# Set the PWM output we are using for the LED
+LED = 18
+
+def setup():
+    global pwm
+
+    # GPIO uses broadcom numbering (GPIO numbers)
+    GPIO.setmode(GPIO.BCM)
+
+    # Set the LED pin as an output
+    GPIO.setup(LED, GPIO.OUT)
+
+    # Start PWM on the LED pin at 200Hz with a
+    # 100% duty cycle. At lower frequencies the LED # would  icker even when we wanted it on solidly pwm = GPIO.PWM(LED, 200)
+    # Start at a brightness of 100%
+    pwm.start(100)
+
+def set_brightness(new_brightness):
+    # Sets brightness of the LED by changing duty cycle
+    pwm.ChangeDutyCycle(new_brightness)
+
+def flicker():
+    # We want a random brightness between 0% and 100%. # Then then we’ll hold it for a random time
+    # between 0.01 and 0.1 seconds to get a nice  icker # effect. Play with these values to make the effect # suit your liking
+    set_brightness(random.randrange(0, 100))
+    time.sleep(random.randrange(1, 10) * 0.01)
+
+# The wrapper around the  icker function makes sure the
+# GPIO hardware is cleaned up when the user presses CTRL-C
+def loop():
+    try:
+        while True:
+            flicker()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        GPIO.cleanup()
+
+# setup the hardware
+setup()
+
+# start the  ickering
+loop()
